e6861d48f6cebcf5a40e2db26f056cb9436acbee
Pablo Moyano <pabmoy1725@gmail.com>
2023-11-30 15:48:01 -0300

    Agregamos un contacto nuevo en un array llamado agenda

--- js/app.js
+++ js/app.js
@@ -1,35 +1,55 @@
 import Contacto from "./classContacto.js";
+//Create - Read - Update - Delete contactos
 
-console.log('prueba desde app.js')
+console.log("prueba desde app.js");
 // const contacto = new Contacto(1, 'Juan', 'Pérez', 'juan.perez@email.com', '555-123-4567');
 
-const contacto = new Contacto(1, 'Juan', 'Pérez', 'juan.perez@email.com', '555-123-4567');
-console.log(contacto)
-const modalAdminContacto = new bootstrap.Modal(document.getElementById('administrarContacto'));
-const btnAgregarContacto = document.getElementById('btnNuevoContacto');
-const formularioContacto = document.querySelector('form');
-const nombre = document.getElementById('nombre'),
- apellido = document.getElementById('apellido'),
- telefono = document.getElementById('telefono'),
- email = document.getElementById('email');
+const contacto = new Contacto(
+  1,
+  "Juan",
+  "Pérez",
+  "juan.perez@email.com",
+  "555-123-4567"
+);
+console.log(contacto);
+const modalAdminContacto = new bootstrap.Modal(
+  document.getElementById("administrarContacto")
+);
+const btnAgregarContacto = document.getElementById("btnNuevoContacto");
+const formularioContacto = document.querySelector("form");
+const nombre = document.getElementById("nombre"),
+  apellido = document.getElementById("apellido"),
+  telefono = document.getElementById("telefono"),
+  email = document.getElementById("email");
+const agenda = [];
 
 //funciones
-const mostrarModal = ()=>{
-    modalAdminContacto.show();
-}
+const mostrarModal = () => {
+  modalAdminContacto.show();
+};
 
-const crearContacto = (e) =>{
-    e.preventDefault();
-    console.log('aqui debo crear el contacto nuevo')
-    //verificar que los datos sean validos
+const crearContacto = (e) => {
+  e.preventDefault();
+  console.log("aqui debo crear el contacto nuevo");
+  //verificar que los datos sean validos
 
-    //crearia el contacto
-    const nuevoContacto = new Contacto(undefined, nombre.value, apellido.value, email.value, telefono.value);
-    console.log(nuevoContacto)
+  //crearia el contacto
+  const nuevoContacto = new Contacto(
+    undefined,
+    nombre.value,
+    apellido.value,
+    email.value,
+    telefono.value
+  );
+  console.log(nuevoContacto);
 
-    //resetear el formulario
-}
+//agrego el contacto nuevo al array
+agenda.push(nuevoContacto);
+
+//resetear el formulario
+
+};
 
 //logica extra
-btnAgregarContacto.addEventListener('click', mostrarModal);
-formularioContacto.addEventListener('submit', crearContacto)
+btnAgregarContacto.addEventListener("click", mostrarModal);
+formularioContacto.addEventListener("submit", crearContacto);
